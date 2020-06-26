9d75f8ccc765485994123bd86250240d65b9e34a
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-26 07:57:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var kittens = [] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(name)
+function destructivelyAppendKitten(name){
+
+}
 
 function destructivelyPrependKitten(name) {
 
